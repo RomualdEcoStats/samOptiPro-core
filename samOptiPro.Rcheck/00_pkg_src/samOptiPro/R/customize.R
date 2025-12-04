3bb41961cdb97f9fbd5f0e392091882762ada493
Romuald Hounyeme <romualdaurel@outlook.com>
2025-12-04 18:37:51 +0100

    fix: regenerate documentation and Rd files

--- samOptiPro.Rcheck/00_pkg_src/samOptiPro/R/customize.R
+++
@@ -1,5 +0,0 @@
-## ---- Internal functions (1) ----
-
-customize_samplers <- function(conf, low_ess=NULL, strategy=list(sampler_for_low_ess='slice', block_correlated=TRUE, block_size=3)){ if(!is.null(low_ess)) for(nm in low_ess){ if(conf$isSamplerAssigned(nm)) conf$removeSampler(nm); conf$addSampler(nm, type='slice') } ; conf }
-
-
